4a96a30485def115f0438b5f4089fd71ab789976
Pradeep Kumar <dm.pradeep000@gmail.com>
2023-10-03 19:37:05 +0530

    Update History.js

--- src/History/History.js
+++ src/History/History.js
@@ -1,21 +1,24 @@
-import React, { useState } from "react";
+import React from "react";
 import './history.css';
 import { useSelector } from 'react-redux';
+import { Link } from "react-router-dom"; // Import Link from react-router-dom
 
 export default function History() {
+    const history = useSelector((state) => state.history.history);
 
-    const history = useSelector((state) => state.history.history)
     return (
         <div className="history">
             <h1>Searched History</h1>
             <ul>
                 {history.map((item, index) => (
                     <li key={index}>
-                        <a> {item.word} </a>
+                        {/* Use Link for redirection */}
+                        <Link to={`/search/${item.word}`} className="underline font-24 mx-4 my-2 text-blue-700 w-min">
+                            {item.word}
+                        </Link>
                     </li>
                 ))}
             </ul>
-
         </div>
     );
 }
